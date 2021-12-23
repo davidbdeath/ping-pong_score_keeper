ab7e9b74b97ebfb91da0229c65b7668a5b90392c
davidbdeath <social_davidbdyer@icloud.com>
2021-12-23 23:52:02 +0000

    Match display updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,32 +1,31 @@
 const p1 = {
 	playerName: 'Player 1',
 	score: 0,
-	matches: 0,
+	matchesWon: 0,
 	button: document.querySelector('#button-p1'),
 	scoreDisplay: document.querySelector('input[name=score-p1]'),
-	matchDisplay: document.querySelector('input[name=match-p1]')
 }
 
 const p2 = {
 	playerName: 'Player 2',
 	score: 0,
-	matches: 0,
+	matchesWon: 0,
 	button: document.querySelector('#button-p2'),
 	scoreDisplay: document.querySelector('input[name=score-p2]'),
-	matchDisplay: document.querySelector('input[name=match-p2]')
 }
 
 const winnerModal = document.querySelector('#winner-modal');
 
 // document buttons
-const resetButton = document.querySelector('#reset');
-const nextRound = document.querySelector('#next-round');
-const newGame = document.querySelector('#new-game');
+const resetBtn = document.querySelector('#reset');
+const nextRoundBtn = document.querySelector('#next-round');
+const newGameBtn = document.querySelector('#new-game');
+const matchRoundDisplay = document.querySelector('input[name=match-round]')
+const matchGameDisplay = document.querySelector('input[name=match-game]')
 
 const winningScore = 11;
-
+let winningMatch = 5;
 let isGameOver = false;
-let round = 1;
 
 // setters
 function setScore(player, value) {
@@ -35,8 +34,12 @@ function setScore(player, value) {
 
 function setDisplay(player, elm, value) {
 	player[elm].value = value;
-	player[elm].value = value;
 }
+
+function setPlayerProp(player, prop, value) {
+	player[prop].value = value;
+}
+
 
 function updateScores(player) {
 	if (!isGameOver) {
@@ -44,20 +47,6 @@ function updateScores(player) {
 		let newScore = player.score + 1;
 		setScore(player, newScore);
 		setDisplay(player, 'scoreDisplay', newScore);
-	}
-}
-
-function setGameOver(player1, player2, winScore) {
-	if (player1.score >= winScore || player2.score >= winScore) {
-		if (player1.score - player2.score >= 2 || player2.score - player1.score >= 2) {
-			isGameOver = true;
-			if (player1.score > player2.score) {
-				updateModal(winnerModal, player1.playerName, round)
-			} else {
-				updateModal(winnerModal, player2.playerName, round)
-			}
-			toggleModal(winnerModal, true);
-		}
 	}
 }
 
@@ -74,6 +63,28 @@ function toggleModal(elm, showBool) {
 	}
 }
 
+function setGameOver(player1, player2, winScore) {
+	round = matchRoundDisplay.value
+	if (player1.score >= winScore || player2.score >= winScore) {
+		if (player1.score - player2.score >= 2 || player2.score - player1.score >= 2) {
+			isGameOver = true;
+			if (player1.score > player2.score) {
+				updateModal(winnerModal, player1.playerName, round)
+			} else {
+				updateModal(winnerModal, player2.playerName, round)
+			}
+			toggleModal(winnerModal, true);
+		}
+	}
+}
+
+function newGame() {
+	setScore(p1, 0);
+	setScore(p2, 0);
+	setDisplay(p1, 'scoreDisplay', 0);
+	setDisplay(p2, 'scoreDisplay', 0);
+	isGameOver = false;
+}
 
 // event listners
 p1.button.addEventListener('click', () => {
@@ -86,19 +97,41 @@ p2.button.addEventListener('click', () => {
 	setGameOver(p1, p2, winningScore);
 })
 
-resetButton.addEventListener('click', () => {
-	setScore(p1, 0);
-	setScore(p2, 0);
-	setDisplay(p1, 'scoreDisplay', 0);
-	setDisplay(p2, 'scoreDisplay', 0);
+resetBtn.addEventListener('click', () => {
+	matchRoundDisplay.value = 1;
+	matchRoundDisplay.style.color = 'black';
+	newGame();
 	isGameOver = false;
 })
 
-newGame.addEventListener('click', () => {
-	setScore(p1, 0);
-	setScore(p2, 0);
-	setDisplay(p1, 'scoreDisplay', 0);
-	setDisplay(p2, 'scoreDisplay', 0);
-	isGameOver = false;
+newGameBtn.addEventListener('click', () => {
+	newGame();
 	toggleModal(winnerModal, false);
 });
+
+nextRoundBtn.addEventListener('click', () => {
+	round = matchRoundDisplay.value++;
+	if (p1.score > p2.score) {
+		setPlayerProp(p1, 'matchesWon', p1.matchesWon++)
+	} else {
+		setPlayerProp(p2, 'matchesWon', p2.matchesWon++)
+	}
+	match()
+	newGame();
+	toggleModal(winnerModal, false);
+});
+
+function match() {
+	if (p1.matchesWon > p2.matchesWon) {
+		matchRoundDisplay.style.color = 'red'
+	} else if (p1.matchesWon < p2.matchesWon) {
+		matchRoundDisplay.style.color = 'blue'
+	} else {
+		matchRoundDisplay.style.color = 'black'
+	}
+}
+
+function updateMatch(player, num) {
+	if (player.matchesWon = num) {
+	}
+}
